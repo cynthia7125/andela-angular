678a08c0ebd5bfed564519daeaf0d907289d0fce
Reyhana Cherop <reyhanacynthia@gmail.com>
2023-08-06 11:27:31 +0300

    Hooks and Router

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,13 +1,38 @@
 import "./main-page.css";
 import Header from "./header";
+import { useEffect, useMemo, useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
+  const [allHouses, setAllHouses] = useState([]);
+  useEffect(() => {
+    const fetchHouses = async () => {
+      const rsp = await fetch("/houses.json");
+      const houses = await rsp.json();
+      setAllHouses(houses);
+    };
+    fetchHouses();
+  }, []);
+
+  const featuredHouse = useMemo(() => {
+    if (allHouses.length) {
+      const randomIndex = Math.floor(Math.random() * allHouses.length);
+      return allHouses[randomIndex];
+    }
+  }, [allHouses]);
+
   return (
-    <div className="container">
-      <Header
-        subtitle="Providing houses all over the world"
-      />
-    </div>
+    <Router>
+      <div className="container">
+        <Header subtitle="Providing houses all over the world" />
+
+        <Switch>
+          <Route path="/">
+
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
